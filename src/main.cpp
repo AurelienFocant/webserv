3228fd117cfc8bf5e77220c77107b09786da9818
agaland <agaland@student.s19.be>
2025-11-21 02:17:27 +0100

    segfault after refresh bye

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define MAX_EVENTS 1024
+
 std::string response = "HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: 637\r\nCache-Control: s-maxage=300, public, max-age=0\r\nContent-Language: en-US\r\nDate: Thu, 06 Dec 2018 17:37:18 GMT\r\nETag: \"2e77ad1dc6ab0b53a2996dfd4653c1c3\"\r\nServer: meinheld/0.6.1\r\nStrict-Transport-Security: max-age=63072000\r\nX-Content-Type-Options: nosniff\r\nX-Frame-Options: DENY\r\nX-XSS-Protection: 1; mode=block\r\nVary: Accept-Encoding,Cookie\r\nAge: 7\r\n\r\n<!doctype html>\r\n<html lang=\"en\">\r\n<head>\r\n<meta charset=\"utf-8\">\r\n<title>A basic webpage</title>\r\n</head>\r\n<body>\r\n<h1>Basic HTML webpage</h1>\r\n<p>Hello, world!</p>\r\n</body>\r\n</html>\r\n";
 
 int	setUpServer()
@@ -27,7 +29,8 @@ int	setUpServer()
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	server_addr.sin_port = htons(8080);
 
-	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	//serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	serverSocket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0); // atomique, a la place d'utiliser fcntl avec O_NONBLOCK
 	if (serverSocket < 0)
 	{
 			perror("serverSocket: ");
@@ -58,12 +61,13 @@ int	main()
 {
 	int listenSocket;
 	std::vector<struct pollfd> pollRequests;
-	struct epoll_event ev, ready_events[10];
+	struct epoll_event ev, ready_events[MAX_EVENTS];
 	
 	listenSocket = setUpServer();
 	if (listenSocket < 0)
 		return (1);
-	fcntl(listenSocket, F_SETFL, O_NONBLOCK);
+
+	//fcntl(listenSocket, F_SETFL, O_NONBLOCK);
 
 	int e_fd = epoll_create1(0);
 	std::cout << "epoll fd: " << e_fd << std::endl;
@@ -75,51 +79,32 @@ int	main()
 
 	ev.events = EPOLLIN;
 	ev.data.fd = listenSocket;
-
 	epoll_ctl(e_fd, EPOLL_CTL_ADD, listenSocket, &ev);
-
-	struct pollfd pollFd;
-	memset(&pollFd, 0, sizeof(pollFd));
-	pollFd.fd = listenSocket;
-	pollFd.events = POLLIN;
-	pollFd.revents = 0;
-	pollRequests.push_back(pollFd);
-	
 
 	while (1) {
 		int	clientFd = 0;
 		struct sockaddr_in	client_addr;
 		socklen_t client_addr_len = sizeof(client_addr);
 		char ip_buf[INET_ADDRSTRLEN];
-		char response_to_parse[102400];
+		char request_to_parse[102400];
 
-		int efd_count = epoll_wait(e_fd, ready_events, sizeof(ready_events), -1);
+		int efd_count = epoll_wait(e_fd, ready_events, MAX_EVENTS, 100);
 		if (efd_count < 0)
 			perror("epoll_wait: ");
 
-		int pollCount = poll(pollRequests.data(), pollRequests.size(), 1000);
-		if (pollCount < 0)
-			perror("poll: ");
-		
-		for (size_t i = 0; i < pollRequests.size(); i++)
+		for (int i = 0; i < efd_count; i++)
 		{
-			if (!(pollRequests[i].revents & POLLIN))
-				continue;
-			else 
-				std::cout << "pollin happened" << std::endl;
-
-			if (pollRequests[i].fd == listenSocket)
+			std::cout << "READY FD: " << ready_events[i].data.fd << std::endl;
+			 
+			if (ready_events[i].data.fd == listenSocket)
 			{
 				clientFd = accept(listenSocket, (struct sockaddr *) &client_addr, &client_addr_len);
 				if (clientFd < 0)
 					perror("accept: ");
-		
-				struct pollfd pollFd;
-				memset(&pollFd, 0, sizeof(pollFd));
-				pollFd.fd = clientFd;
-				pollFd.events = POLLIN;
-				pollFd.revents = 0;
-				pollRequests.push_back(pollFd);
+				
+				ev.events = EPOLLIN;
+				ev.data.fd = clientFd;
+				epoll_ctl(e_fd, EPOLL_CTL_ADD, clientFd, &ev);
 
 				/* PRINT LOG CLIENT */
 				std::cout << "client fd : " << clientFd << std::endl;
@@ -128,11 +113,12 @@ int	main()
 			}
 			else
 			{
+
 				ssize_t	ret;
 				//std::string	request;
-				ret = recv(pollRequests[i].fd, (void *) response_to_parse, sizeof(response_to_parse), 0);
-				std::cout << response_to_parse << std::endl;
-					//request.append(response_to_parse);
+				ret = recv(ready_events[i].data.fd, (void *) request_to_parse, sizeof(request_to_parse), 0);
+				std::cout << request_to_parse << std::endl;
+					//request.append(request_to_parse);
 				if (ret <=0)
 				{
 					if (ret < 0)
@@ -140,16 +126,16 @@ int	main()
 					else
 						std::cout << "Connection " << pollRequests[i].fd << " closed" << std::endl;
 					continue ;
+					// if erno -1 sur EAGAIN / EWOULDBLOCK continue;
 				}
 				// -> parser la requete http blablabla
-
-				if ((send(pollRequests[i].fd, (const void *) response.c_str(), response.length(), MSG_NOSIGNAL)) < 0)
+				if ((send(ready_events[i].data.fd, (const void *) response.c_str(), response.length(), MSG_NOSIGNAL)) < 0)
 					perror("send: ");
-				// ! send() peut renvoyer - que response.lenght / envoi partiel
 
-/* 				close(pollRequests[i].fd);
-				pollRequests.erase(pollRequests.begin() + i);
-				i--; */
+				std::cout << "PROBLEMOS?" << std::endl;
+
+				// if erno -1 sur EAGAIN / EWOULDBLOCK continue;
+				// ! send() peut renvoyer - que response.lenght / envoi partiel
 			}
 		}
 
