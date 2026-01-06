3e8f1588932c69464cec746d2eb1a7e053b1cd1b
Simon Tempels <stempels@shi-r12-p1.s19.be>
2026-01-06 15:19:52 +0100

    compiling now

--- src/Response.hpp
+++ src/Response.hpp
@@ -31,20 +31,20 @@ class	Response
 	private:
 
 	/*Private Attributes*/
-		state			_state;
-		t_HttpCode		_status_code;
-		bodyType		_type;
+	//	state			_state;
+	//	t_HttpCode		_status_code;
+	//	bodyType		_type;
 
 		//std::string		_headers;
 		//size_t			_headers_sent;
-		
+/*		
 		int				_fd;
 		size_t			_body_size;
 		size_t			_body_sent;
 		char			_buffer[8192]; // or std::vector<char> ? for CGI?
 		size_t			_buffer_sent;
 		size_t			_buffer_size;
-
+*/
 
 	/*Private Methods*/
 
