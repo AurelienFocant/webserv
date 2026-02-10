7b25257f6c89cf8aab055ba6c8553f79e3e03b1c
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2026-02-10 19:19:08 +0100

    c'est beau c'est bien alligné

--- src/Config/ConfigBuild/ConfigContext.hpp
+++ src/Config/ConfigBuild/ConfigContext.hpp
@@ -39,41 +39,42 @@ class ConfigContext {
 		void	inheritFrom(const ConfigContext &parent);
 
 
-		ContextType getType(void) const;
-		int         getPort(void) const;
-		void        setPort(int port);
-		std::string getRoot(void) const;
-		void        setRoot(const std::string &root);
-		std::string getServerName(void) const;
-		void        setServerName(const std::string &serverName);
-		std::string getLocationName(void) const;
-		void		setLocationName(const std::string &locationName);
-		const std::map<std::string, Location>&	getLocations(void) const;
-		void									addLocation(const std::string &name, const Location &loc);
-		void							setIndexes(std::vector<std::string> const& src);
-		std::vector<std::string> const&	getIndexes(void) const;
-		bool		getAutoindex(void) const;
-		void		setAutoindex(bool b)		{_autoindex = b;}
-		int			getRedirectCode(void) const	{return _redirect_code;}
-		void		setRedirectCode(int code)	{_redirect_code = code;}
-		std::string	getRedirect(void) const		{return _redirect;}
-		void		setRedirect(std::string redirect)	{_redirect = redirect;}
-		std::time_t	getKeepalive_time() const			{return _keepalive_time;}
-		void		setKeepalive_time(std::time_t t)	{_keepalive_time = t;}
-		std::time_t	getKeepalive_timeout() const		{return _keepalive_timeout;}
-		void		setKeepalive_timeout(std::time_t t) {_keepalive_timeout = t;}
-		std::set<std::string>	getAllowedMethods(void) const	{return _allowed_methods;}
-		void					setAllowedMethods(std::set<std::string> m) {_allowed_methods = m;}
-		void		setCGI(bool b) {_cgi = b;}
-		bool		getCGI() const {return _cgi;}
-		void		setVirtualLocation(bool b) {_virtualLocation = b;}
-		bool		getVirtualLocation() const {return _virtualLocation;}
-		void		setAlias(std::string a) {_alias = a;}
-		std::string	getAlias() const {return _alias;}
-		void		setErrorPages(std::map<int, std::string> ep) {_error_pages = ep;}
-		std::map<int, std::string>	getErrorPages() const {return _error_pages;}
-		void		setErrorPage(int code, std::string page) {_error_pages.insert(std::make_pair(code, page));}
-		std::string	getErrorPage(int code) const {return _error_pages.at(code);}
+									ContextType getType()				const	{return _type;}
+									int         getPort()				const	{return _port;}
+									std::string getRoot()				const	{return _root;}
+									std::string getServerName()			const	{return _serverName;}
+									std::string getLocationName()		const	{return _locationName;}
+		 std::map<std::string, Location> const& getLocations()			const	{return _locations;}
+									bool		getAutoindex() 			const	{return _autoindex;}
+				std::vector<std::string> const&	getIndexes()   			const	{return _indexes;}
+									int			getRedirectCode()		const	{return _redirect_code;}
+									std::string	getRedirect()			const	{return _redirect;}
+									std::time_t	getKeepalive_time()		const	{return _keepalive_time;}
+									std::time_t	getKeepalive_timeout()	const	{return _keepalive_timeout;}
+						std::set<std::string>	getAllowedMethods()		const	{return _allowed_methods;}
+									bool		getCGI()	   			const	{return _cgi;}
+									bool		getVirtualLocation()	const	{return _virtualLocation;}
+									std::string	getAlias()				const	{return _alias;}
+					std::map<int, std::string>	getErrorPages()	   		const	{return _error_pages;}
+									std::string	getErrorPage(int code)  const	{return _error_pages.at(code);}
+
+		void        setPort(int port)											{_port = port;}
+		void        setRoot(const std::string &root)							{_root = root;}
+		void        setServerName(const std::string &serverName)				{_serverName = serverName;}
+		void		setLocationName(const std::string &locationName)			{_locationName = locationName;}
+		void		addLocation(const std::string &name, const Location &loc)	{_locations.insert(std::make_pair(name, loc));}
+		void		setIndexes(std::vector<std::string> const& src)				{_indexes.assign(src.begin(), src.end());}
+		void		setAutoindex(bool b)										{_autoindex = b;}
+		void		setRedirectCode(int code)									{_redirect_code = code;}
+		void		setRedirect(std::string redirect)							{_redirect = redirect;}
+		void		setKeepalive_time(std::time_t t)							{_keepalive_time = t;}
+		void		setKeepalive_timeout(std::time_t t) 						{_keepalive_timeout = t;}
+		void		setAllowedMethods(std::set<std::string> m)					{_allowed_methods = m;}
+		void		setCGI(bool b)												{_cgi = b;}
+		void		setVirtualLocation(bool b)									{_virtualLocation = b;}
+		void		setAlias(std::string a)										{_alias = a;}
+		void		setErrorPages(std::map<int, std::string> ep)				{_error_pages = ep;}
+		void		setErrorPage(int code, std::string page)					{_error_pages.insert(std::make_pair(code, page));}
 
 		ConfigContext(void);
 		ConfigContext(ContextType t);
