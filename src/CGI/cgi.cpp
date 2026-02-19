ebedc1afc9830c9ba5c2713da025bb01c4079199
Simon Tempels <stempels@shi-r12-p1.s19.be>
2026-02-19 11:29:41 +0100

    c_enc nullified

--- src/CGI/cgi.cpp
+++ src/CGI/cgi.cpp
@@ -57,7 +57,8 @@ char**	cgi::buildCgiEnv(const RequestHandler& handler) {
 				header += "HTTP_" + it->first + "=" + it->second;
 		}
 	}
-	char **c_enc = new char*[vect.size()];
+	char **c_enc = new char*[vect.size() + 1];
+	c_enc[vect.size()] = NULL;
 	for (size_t i = 0; i < vect.size(); ++i) {
 		c_enc[i] = new char[vect.at(i).size() + 1];
 		std::strcpy(c_enc[i], vect.at(i).c_str());
