edc38dc49453fbb431aa10656d556c61622022bd
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2026-02-21 17:08:53 +0100

    cleaned VirtualServer files

--- src/Webserv/VirtualServer.hpp
+++ src/Webserv/VirtualServer.hpp
@@ -29,31 +29,29 @@ class VirtualServer
 		std::map<std::string, Location>	_locations;
 
 	public:
-		const std::map<std::string, Location>	&getLocations(void) const;
-		void									setLocations(const std::map<std::string, Location> &locs);
-		void								 	setLocationsAt(std::string key, Location &loc);
-		const Location							&getLocationAt(std::string key) const;
+		void	setPort(unsigned int port)									{_port = port;}
+		void	setRoot(std::string root)									{_root = root;}
+		void	setServName(std::string name)								{_server_name = name;}
+		void	setAutoindex(bool b)										{_autoindex = b;}
+		void	setAlias(std::string a)										{_alias = a;}
+		void	setIndexes(std::vector<std::string> const& src)				{_indexes.assign(src.begin(), src.end());}
+		void	setErrorPages(std::map<int, std::string> ep)				{_error_pages = ep;}
+		void	setErrorPage(int code, std::string page)					{_error_pages.insert(std::make_pair(code, page));}
+		void	setLocations(const std::map<std::string, Location> &locs)	{_locations = locs;}
+		void	setLocationsAt(std::string key, Location &loc)				{_locations.insert(std::make_pair(key, loc));}
 
-		// getReferences or getValues ??
-		void								setPort(unsigned int port);
-		unsigned int						getPort(void) const;
-		void								setRoot(std::string root);
-		std::string							getRoot(void) const;
-		void								setServName(std::string name);
-		std::string							getServName(void) const;
-		void								setIndexes(std::vector<std::string> const& src);
-		std::vector<std::string> const&		getIndexes(void) const;
-		bool								getAutoindex(void) const;
-		void	   	 						setAutoindex(bool b);
-		void		setAlias(std::string a) {_alias = a;}
-		std::string	getAlias() const {return _alias;}
-		void		setErrorPages(std::map<int, std::string> ep) {_error_pages = ep;}
-		std::map<int, std::string> const&	getErrorPages() const {return _error_pages;}
-		void		setErrorPage(int code, std::string page) {_error_pages.insert(std::make_pair(code, page));}
-		std::string	getErrorPage(int code) const {return _error_pages.at(code);}
-
-		std::time_t	getKeepaliveTime(void) const	{return _keepalive_time;}
-		std::time_t	getKeepaliveTimeout(void) const	{return _keepalive_timeout;}
+		unsigned int	getPort(void)							const	{return _port;}
+		std::string		getRoot(void)							const	{return _root;}
+		std::string		getServName(void)						const	{return _server_name;}
+		bool			getAutoindex(void)						const	{return _autoindex;}
+		std::string		getAlias()								const	{return _alias;}
+		std::string		getErrorPage(int code)					const	{return _error_pages.at(code);}
+		std::time_t		getKeepaliveTime(void)					const	{return _keepalive_time;}
+		std::time_t		getKeepaliveTimeout(void)				const	{return _keepalive_timeout;}
+		std::map<int, std::string>		const&	getErrorPages()	const	{return _error_pages;}
+		std::vector<std::string>		const&	getIndexes(void)const	{return _indexes;}
+		std::map<std::string, Location>	const&	getLocations(void)				const	{return _locations;}
+		Location						const&	getLocationAt(std::string key)	const	{return _locations.at(key);}
 
 		VirtualServer	( void );
 		VirtualServer	(ConfigContext const& ctxt);
